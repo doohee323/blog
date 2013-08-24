6eab935ebccde941cbc4039f777bb9d07f3b05b0
Dewey Hong <doohee323@gmail.com>
2013-08-25 08:05:09 +0900

    11

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,16 @@ class CommentsController < ApplicationController
 		redirect_to post_path(@post)
   end
 
+  def update
+    # attr_accessiable :name과 동일
+    post_params = params.require(:post).permit(:body)
+    if @post.update_attributes(post_params)
+      redirect_to @post, notice: 'Updated'
+    else
+      render action: 'edit'
+    end
+  end
+
   def destroy
   	@post = Post.find(param[:post_id])
 		@comment = @post.comments.find(params[:id])
