6a9355375b8ea816e11a3f53d0db0f2923f535f4
Dewey Hong <doohee323@gmail.com>
2013-08-25 19:29:56 +0900

    111

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-     has_many :comments
+     has_many :comments, dependent: :destroy
      has_many :tags
 	 accepts_nested_attributes_for :tags, :allow_destroy => true, 
 		:reject_if => proc { |attrs| attrs.all? {|k,v|v.blank?} }
